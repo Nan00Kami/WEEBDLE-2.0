91941e55a4d819ede38d972b0fe8409bd95d70ed
Nan00Kami <gokulnanoo@gmail.com>
2026-09-10 21:00:00 +0530

    feat: add character portraits and initialize animeData2.js

--- animeData2.js
+++ animeData2.js
@@ -1,2 +1,12 @@
 // animeData2.js - Master Character Catalog
-const MASTER_CHARACTER_DATA = [];
+const MASTER_CHARACTER_DATA = [
+  {
+  id: 1,
+  name: "Satoru Gojo",
+  anime: "Jujutsu Kaisen",
+  genres: ["Action", "Fantasy"],
+  gender: "Male",
+  beatsGoku: true,
+  image: "images/characters/Gojo Satoru.jpg"
+}
+];
